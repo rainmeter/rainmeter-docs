9b31b1eee86d862030a926afd4598ac359935550
Michael Engard <Kaelri@gmail.com>
2012-10-29 19:00:50 -0400

    Expanded local viewing.

--- js/_local.js
+++ js/_local.js
@@ -5,9 +5,29 @@
 
 	document.write(
 		'<link href="css/main.css" rel="stylesheet">' +
-		'<div class="container"><div id="content">' +
-		'<header><h1>' + title + '</h1></header>' +
-		'<article>');
+		'<body>' +
+		'	<div class="wrapper">' +
+		'		<div class="top">' +
+		'			<div class="container">' +
+		'				<div id="logo"></div>' +
+		'				<nav id="nav">' +
+		'					<ul>' +
+		'					<li><a href="//rainmeter.net">Home</a>' +
+		'					<li><a href="//rainmeter.net/cms/About">About</a>' +
+		'					<li><a href="/" class="active">Docs</a>' +
+		'					<li><a href="//rainmeter.net/cms/Discover">Discover</a>' +
+		'					<li><a href="//rainmeter.net/cms/Develop">Develop</a>' +
+		'					<li><a href="//rainmeter.net/forum">Forum</a>' +
+		'					</ul>' +
+		'				</nav>' +
+		'			</div>' +
+		'		</div>' +
+		'		<div class="container">' +
+		'			<div class="row">' + 
+		'				<div id="side-content" style="height: 400px;"></div>' +
+		'				<div id="content">' +
+		'					<header><h1>' + title + '</h1></header>' +
+		'					<article>');
 
 	// Fix highlight blocks.
 	window.addEventListener('load', function() {
